2894a291fcc786e2e0234769c48686a94dd061a5
Anushka <95957335+anushka3002@users.noreply.github.com>
2023-03-14 23:23:12 +0530

    made hover changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 function App() {
   const [showSecondList, setShowSecondList] = useState(0);
   const [showThirdList, setShowThirdList] = useState(0);
-  const [hamburger,setHamburger] = useState(true)
+  const [hamburger, setHamburger] = useState(true);
 
   let dropdownData = [
     {
@@ -123,81 +123,186 @@ function App() {
 
   return (
     <div>
-      <div className="lg:flex md:block text-[#57575c]">
-        <div className="flex justify-between lg:h-[100px]">
-          <img
-            src="https://kharedobecho.com/images/kharedobecho.png"
-            width="120px"
-          ></img>
-          <img onClick={()=>setHamburger(!hamburger)} className="lg:hidden cursor-pointer" src="https://cdn.iconscout.com/icon/free/png-256/hamburger-menu-462145.png" width="40px"></img>
+      <div className=" text-[#57575c]">
+        <div className="flex bg-[#f0f7ff] flex justify-between lg:h-[115px] w-full px-4">
+          <div className="">
+            <img
+              className="pt-2 hidden lg:flex"
+              src="https://kharedobecho.com/images/kharedobecho.png"
+              width="120px"
+              height="100px"
+            ></img>
+            <img
+              className="flex lg:hidden mt-2"
+              src="https://kharedobecho.com/images/kbMobileLogo.png"
+              width="40px"
+              height="40px"
+            ></img>
+          </div>
+          <div className="lg:hidden flex justify-between w-[50%] my-auto">
+            <img
+              className="w-6 h-6"
+              src="https://cdn-icons-png.flaticon.com/512/3917/3917132.png"
+            ></img>
+            <img
+              className="w-6 h-6"
+              src="https://cdn-icons-png.flaticon.com/512/3388/3388641.png"
+            ></img>
+            <img
+              className="w-6 h-6"
+              src="https://www.pikpng.com/pngl/b/283-2837538_login-icon-line-icons-iconscout-login-icon-images.png"
+            ></img>
+            <img
+              onClick={() => setHamburger(!hamburger)}
+              className="w-6 h-6 cursor-pointer"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png"
+            ></img>
+          </div>
+          <div className="lg:flex hidden flex h-[45px] mt-4 pr-4">
+            <div className=" bg-[white] flex w-[270px] justify-between items-center justify-center px-2">
+              <div className="flex">
+                <img
+                  className="w-[20px] h-[20px]"
+                  src="https://www.svgrepo.com/show/127575/location-sign.svg"
+                ></img>
+                <p>India</p>
+              </div>
+              <img
+                className="w-[15px] h-[15px]"
+                src="https://cdn-icons-png.flaticon.com/512/25/25623.png"
+              ></img>
+            </div>
+            <select className="lg:flex hidden px-2 border-[#d8d8d8] border-x-[2px]">
+              <option>Choose Services Near You</option>
+            </select>
+            <input
+              placeholder="What are you looking for"
+              className="border px-2 w-[300px] lg:flex hidden"
+            ></input>
+            <div className="border bg-[black] w-[80px] rounded-r-[4px] lg:flex hidden">
+              <img
+                className="w-[25px] h-[25px] mx-auto py-auto mt-2"
+                src="https://png.pngtree.com/png-vector/20190321/ourmid/pngtree-vector-find-icon-png-image_854997.jpg"
+              ></img>
+            </div>
+            <button className="hidden lg:flex border-[2px] border-[#6acb00] rounded-[5px] flex h-[35px] my-auto items-center p-4 ml-3">
+              <img
+                className="w-[15px] h-[15px]"
+                src="https://cdn-icons-png.flaticon.com/512/3388/3388641.png"
+              ></img>
+              <p className="text-[14px] font-medium ml-1">Post Adv</p>
+            </button>
+            <button className="lg:flex hidden border-[2px] border-[#6acb00] rounded-[5px] flex h-[30px] my-auto items-center p-4 ml-3">
+              <img
+                className="w-[15px] h-[15px]"
+                src="https://www.pikpng.com/pngl/b/283-2837538_login-icon-line-icons-iconscout-login-icon-images.png"
+              ></img>
+              <p className="text-[14px] font-medium ml-1">Login</p>
+            </button>
+          </div>
         </div>
-        <div className={`mt-5 lg:flex ${hamburger?"md:visible":"md:hidden"} mx-auto justify-between lg:w-[70%] md:w-full`}>
+        <div
+          className={`text-[14px] lg:flex w-full lg:mt-[-35px] ${
+            hamburger ? "visible" : "hidden"
+          } mx-auto justify-between lg:w-[100%] md:w-full`}
+        >
           {dropdownData.map((list) => {
             return (
               <div className="lg:w-[18%] md:w-[95%]">
                 <div
-                onMouseOver={() => {
-                  showSecondList === list.id
-                    ? setShowSecondList(0)
-                    : setShowSecondList(list.id); 
-                }}
-                  className="mx-5 cursor-pointer flex lg:pb-2 md:p-2 p-2 w-full md:justify-between justify-content lg:justify-start lg:pt-1 md:border lg:border-[white] lg:border border"
+                  className={`mx-5 cursor-pointer ${
+                    list.secondList.length === 0 && "py-2"
+                  } flex lg:pb-2 justify-between lg:justify-start lg:pt-1 md:border lg:border-[#f0f7ff] lg:border border`}
                 >
-                  <p onMouseOver={() => {
-                    showSecondList === list.id
-                      ? setShowSecondList(0)
-                      : setShowSecondList(list.id); 
-                  }}
-                  className="">{list.firstList}
+                  <p
+                    onMouseOver={() => {
+                      showSecondList === list.id
+                        ? setShowSecondList(0)
+                        : setShowSecondList(list.id);
+                    }}
+                    className="my-auto ml-3 hidden lg:block"
+                  >
+                    {list.firstList}
+                  </p>
+                  <p className="w-full my-auto ml-3 block lg:hidden">
+                    {list.firstList}
                   </p>
                   {list.secondList.length > 0 && (
-                    <img
-                      className="w-[10px] h-[5px] my-auto lg:ml-2 md:mr-6"
-                      src="http://cdn.onlinewebfonts.com/svg/img_218064.png"
-                    ></img>
+                    <div
+                      onClick={() => {
+                        showSecondList === list.id
+                          ? setShowSecondList(0)
+                          : setShowSecondList(list.id);
+                      }}
+                      className="border-l lg:border:none w-[60px] ml-[-20px] my-auto py-4 lg:py-0 mx-auto items-center"
+                    >
+                      <img
+                        className="w-[10px] h-[5px] md:mr-6 mx-auto"
+                        src="http://cdn.onlinewebfonts.com/svg/img_218064.png"
+                      ></img>
+                    </div>
                   )}
                 </div>
+
                 <div className="lg:flex md:block lg:w-[400px] md:w-full">
                   <div
-                    className={`ml-5 cursor-pointer ${
-                      showSecondList === list.id
-                        ? "block"
-                        : "hidden"
+                    className={`mx-5 cursor-pointer ${
+                      showSecondList === list.id ? "block" : "hidden"
                     }`}
                   >
                     {list.secondList.map((e) => {
                       return (
-                        <div
-                          className="lg:flex"
-                        > 
-                          <div className="lg:w-[200px] md:w-full flex justify-between p-2 bg-[#f0f7ff] border">
-                          <p onMouseOver={() => {
-                            showThirdList === e.id 
-                            &&  showSecondList === list.id
-                              ? setShowThirdList(0)
-                              : setShowThirdList(e.id);
-                          }}
-                          
-                           className="transition ease-in-out delay-150 hover:translate-x-1 duration-300 hover:text-[#959498]">{e.secondKey}</p>
-                          {e.list.length > 0 && (
-                            <img
-                              className="w-[10px] h-[10px] mt-2 ml-2"
-                              src="https://cdn-icons-png.flaticon.com/512/32/32213.png"
-                            ></img>
-                          )}
+                        <div className="lg:flex">
+                          <div className="lg:w-[200px] md:w-full flex justify-between p-2 bg-[#f3f7fd]">
+                            <p
+                              onMouseOver={() => {
+                                showThirdList === e.id &&
+                                showSecondList === list.id
+                                  ? setShowThirdList(0)
+                                  : setShowThirdList(e.id);
+                              }}
+                              className="hidden lg:block pl-3 w-full transition ease-in-out delay-150 hover:translate-x-1 duration-300 hover:text-[#959498]"
+                            >
+                              {e.secondKey}
+                            </p>
+                            <p className="block lg:hidden pl-3 w-full transition ease-in-out delay-150 hover:translate-x-1 duration-300 hover:text-[#959498]">
+                              {e.secondKey}
+                            </p>
+                            {e.list.length > 0 && (
+                              <div
+                                onClick={() => {
+                                  showThirdList === e.id &&
+                                  showSecondList === list.id
+                                    ? setShowThirdList(0)
+                                    : setShowThirdList(e.id);
+                                }}
+                                className=""
+                              >
+                                <img
+                                  className="w-[10px] h-[10px] mt-2 ml-2"
+                                  src="https://cdn-icons-png.flaticon.com/512/32/32213.png"
+                                ></img>
+                              </div>
+                            )}
                           </div>
                           <div
                             className={`${
-                              e.list.length > 0 && 
+                              e.list.length > 0 &&
                               showThirdList === e.id &&
                               showSecondList === showSecondList
-                                ? "block border"
+                                ? "block border-l border-[#eeeff1]"
                                 : "hidden"
                             }`}
                           >
                             <div>
                               {e.list.map((el) => {
-                                return <div className="p-2 bg-[#f0f7ff] pl-4"><p className="transition ease-in-out delay-150 hover:translate-x-1 duration-300 hover:text-[#959498]">{el}</p></div>;
+                                return (
+                                  <div className="p-2 bg-[#f0f7ff] pl-4">
+                                    <p className="ml-3 transition ease-in-out delay-150 hover:translate-x-1 duration-300 hover:text-[#959498]">
+                                      {el}
+                                    </p>
+                                  </div>
+                                );
                               })}
                             </div>
                           </div>
